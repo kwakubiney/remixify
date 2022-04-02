233f9ffbf4dce6dda261ed176882cc1fc664151e
Kwaku Biney <kebiney@hotmail.com>
2022-04-02 19:41:54 +0000

    change debug to False for prod

--- main/settings/prod_settings.py
+++ main/settings/prod_settings.py
@@ -3,7 +3,7 @@ import dj_database_url
 from decouple import config
 
 DATABASES = {'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)}
-DEBUG = True
+DEBUG = False
 ROOT_URLCONF = 'main.urls'
 MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 ALLOWED_HOSTS = ['https://remixify-007.herokuapp.com', 'remixify-007.herokuapp.com']
