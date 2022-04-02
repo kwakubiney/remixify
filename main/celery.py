c3ddacbfd8dea976bd42d3c39115bb8a9519c3da
Kwaku Biney <kebiney@hotmail.com>
2022-04-02 20:35:28 +0000

    fix celery env vars

--- main/celery.py
+++ main/celery.py
@@ -2,19 +2,13 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from django.conf import settings
+from decouple import config
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
-
-app = Celery("main" ,)
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.dev_settings")
+app = Celery("main" , broker= settings.REDIS_URL , backend=settings.REDIS_URL)
 app.config_from_object("django.conf.settings", namespace="CELERY")
 
 app.autodiscover_tasks()
-
-#celery beat settings
-app.conf.beat_schedule = {
-
-}
 
 @app.task(bind=True)
 def debug_task(self):
