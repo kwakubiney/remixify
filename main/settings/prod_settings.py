d45a4dd44c5b1666f7fea534c878746a800eb55d
Kwaku Biney <kebiney@hotmail.com>
2022-04-02 18:05:04 +0000

    add urlconf

--- main/settings/prod_settings.py
+++ main/settings/prod_settings.py
@@ -5,6 +5,7 @@ from decouple import config
 
 DATABASES = {'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)}
 DEBUG = False
+ROOT_URLCONF = 'main.urls'
 MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 ALLOWED_HOSTS = ['https://remixify-007.herokuapp.com/', 'remixify-007.herokuapp.com/']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
