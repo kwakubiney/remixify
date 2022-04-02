02fb33c96be9d94a70317b090825cc2eb9424685
Kwaku Biney <kebiney@hotmail.com>
2022-04-02 21:57:21 +0000

    heroku testing

--- main/settings/prod_settings.py
+++ main/settings/prod_settings.py
@@ -8,15 +8,3 @@ ROOT_URLCONF = 'main.urls'
 MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 ALLOWED_HOSTS = ['https://remixify-007.herokuapp.com', 'remixify-007.herokuapp.com']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-CACHES = {
-    "default": {
-        "BACKEND": "redis_cache.RedisCache",
-        "LOCATION": config("REDIS_URL"),
-        "OPTIONS": {
-            "PICKLE_VERSION": 2,
-            "CONNECTION_POOL_CLASS": "redis.ConnectionPool",
-            "CONNECTION_POOL_CLASS_KWARGS": {"max_connections": 1},
-        },
-    }
-}
